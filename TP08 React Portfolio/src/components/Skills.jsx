f2b7edea28629930e12677bcdf6d57f43d6368d7
GABRIEL CHEDIEX VINER <46916327@ort>
2023-10-19 10:42:17 -0300

    Json terminado

--- TP08 React Portfolio/src/components/Skills.jsx
+++ TP08 React Portfolio/src/components/Skills.jsx
@@ -1,14 +1,37 @@
 import { useAppContext } from "../appContext";
 import { Element } from "react-scroll";
+import axios from 'axios';
+import "../App.css"
 // Data
 import { skillData, resume } from "../data";
 // Components
 import { Button, Col, Container, Row } from "react-bootstrap";
 import { Title } from "./globalStyledComponents";
+import { useEffect, useState } from "react";  
 
 export default function Skills() {
+  //const initialSkills = localStorage.getItem("skills") ? JSON.parse(localStorage.getItem("skills")) : [];
+  const [skillsList, setSkillList] = useState([/*initialSkills*/])
   const { theme } = useAppContext();
 
+  useEffect(() => {    
+    const getSkills = async () => {
+      axios
+        .get("Icons.json")
+        .then((result) => {
+          setSkillList(result.data);
+          /*console.log(skillsList)*/
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    };
+    getSkills();      
+  }, []);
+
+  /*useEffect(() => {
+    localStorage.setItem("skills", JSON.stringify(skillsList))      
+  }, [skillsList])*/
   return (
     <Element name={"Skills"} id="skills">
       <section className="section">
@@ -18,13 +41,12 @@ export default function Skills() {
             <div className="underline"></div>
           </Title>
           <Row className="mt-3 align-items-center">
-            {skillData.map((skills) => {
-              return (
-                <Col xs={4} key={skills.id} className="my-md-5">
-                  <figure>
-                    {skills.skill}
-                    <figcaption>{skills.name}</figcaption>
-                  </figure>
+            {skillsList.map((skills, id) => {
+              console.log(skills.length)
+              return(
+               <Col xs={3} key={id} className="my-md-5">
+                    <img className="skill-icon" src={skills.icon} alt={skills.name}/>
+                    <p>{skills.name}</p>
                 </Col>
               );
             })}
--- TP08 React Portfolio/src/data.js
+++ TP08 React Portfolio/src/data.js
@@ -1,6 +1,7 @@
 // Skills icons - https://icon-sets.iconify.design/
 import { Icon } from "@iconify/react";
 
+
 // Navbar Logo image (uncomment below and import your image)
 // import newLogo from "./images/yourFileName"
 
@@ -37,53 +38,7 @@ export const moreInfo =
  ************************************************************** 
   Add or remove skills in the SAME format below, choose icons here - https://icon-sets.iconify.design/
 */
-export const skillData = [
-  {
-    id: 1,
-    skill: <Icon icon="mdi:language-html5" className="display-4" />,
-    name: "HTML5",
-  },
-  {
-    id: 2,
-    skill: <Icon icon="ion:logo-css3" className="display-4" />,
-    name: "CSS3",
-  },
-  {
-    id: 3,
-    skill: <Icon icon="fa6-brands:js" className="display-4" />,
-    name: "JavaScript",
-  },
-  {
-    id: 4,
-    skill: <Icon icon="ri:bootstrap-fill" className="display-4" />,
-    name: "BootStrap",
-  },
-  {
-    id: 5,
-    skill: <Icon icon="mdi:react" className="display-4" />,
-    name: "React",
-  },
-  {
-    id: 6,
-    skill: <Icon icon="file-icons:styledcomponents" className="display-4" />,
-    name: "Styled Components",
-  },
-  {
-    id: 7,
-    skill: <Icon icon="akar-icons:redux-fill" className="display-4" />,
-    name: "Redux",
-  },
-  {
-    id: 8,
-    skill: <Icon icon="bi:git" className="display-4" />,
-    name: "Git",
-  },
-  {
-    id: 9,
-    skill: <Icon icon="fa6-brands:square-github" className="display-4" />,
-    name: "GitHub",
-  },
-];
+
 
 // Resume link (string - "https://YourResumeUrl") - I am using CloudFront to share my resume (https://docs.aws.amazon.com/AmazonCloudFront/latest/DeveloperGuide/Introduction.html)
 export const resume = null;
